51e6ec3b4dea1bd3d16b8c3c423eb757623b19d2
kozae <emma.koza@uberflip.com>
2018-08-19 17:02:20 -0400

    Adjust selector to grab only text elements, add functionality to replace guys with a random gender neutral term

--- content.js
+++ content.js
@@ -1,10 +1,15 @@
-const elements = document.body.getElementsByTagName("*");
+const elements = document.body.querySelectorAll('p,form, button, li, em, a, h1, h2 ,h3, h4, h5, h6, strong, input, label, figcaption');
 const elementsArray = [...elements];
 
+const genderNeutralTerms = ['muggles', 'humans', 'people', 'peeps', 'chums', 'pals', 'beings'];
+
+const randomize = (array) =>{
+   return array[Math.floor(Math.random() * array.length)];
+}
 const updateWebPageText = (elementsArray) => {
   elementsArray.forEach(element => {
     const currentText = element.innerHTML;
-    const replacedText = currentText.replace(/guys/gi, 'muggles');
+    const replacedText = currentText.replace(/guys/gi, (randomize(genderNeutralTerms)));
     element.innerHTML = replacedText;
   }); 
 };
